409b31b49a34bee07d2bf708416bb83bc54fb5ba
Moh Zulkifli Katili <mohzulkiflikatili@gmail.com>
2023-11-25 23:15:22 +0700

    Update __init__.py
    comment asyncio, because it has bug when we use Fast API Uvicorn. 
    
    ValueError: Can't patch loop of type <class 'uvloop.Loop'>
    
    /site-packages/pyrdf2vec/__init__.py", line 6, in <module>

--- pyrdf2vec/__init__.py
+++ pyrdf2vec/__init__.py
@@ -3,7 +3,7 @@ import nest_asyncio
 from .rdf2vec import RDF2VecTransformer
 
 # bypass the asyncio.run error for the Notebooks.
-nest_asyncio.apply()
+# nest_asyncio.apply()
 
 __all__ = [
     "RDF2VecTransformer",
